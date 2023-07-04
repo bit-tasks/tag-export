37dc90603b5f606a47ee1a4e8ed002ff853de4c9
Ashan Fernando <ashan256@gmail.com>
2023-07-04 22:48:21 +0530

    Update tag-export.ts

--- scripts/tag-export.ts
+++ scripts/tag-export.ts
@@ -2,7 +2,7 @@
 export type ExecFunction = (command: string, options?: {cwd: string}) => Promise<number>;
 
 const run: (exec: ExecFunction, wsdir: string) => Promise<void> = async (exec, wsdir) => {
-  await exec('bit tag --persist', { cwd: wsdir });
+  await exec('bit tag -m "CI', { cwd: wsdir });
   await exec('bit export', { cwd: wsdir });
 }
 
