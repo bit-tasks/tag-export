498bd0f8ae25aeb4f955b0c897be7e3139a931da
ashanfernando <ashan256@gmail.com>
2023-07-05 17:51:00 +0530

    Update tag and export

--- dist/index.js
+++ dist/index.js
@@ -3997,7 +3997,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 const run = (exec, wsdir) => __awaiter(void 0, void 0, void 0, function* () {
-    yield exec('bit tag --persist', { cwd: wsdir });
+    yield exec('bit tag -m "CI', { cwd: wsdir });
     yield exec('bit export', { cwd: wsdir });
 });
 exports["default"] = run;
